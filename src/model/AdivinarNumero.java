ee133b6bbbbae0f3fa0d7e3458c40275684c5c0c
Isaiasvm <isaiasvm1993@gmail.com>
2026-03-04 18:38:31 +0100

    logica de adivinar numero
    logica de adivinar el numero completa, o eso creo ahora mismo

--- src/model/AdivinarNumero.java
+++ src/model/AdivinarNumero.java
@@ -14,7 +14,7 @@ public class AdivinarNumero extends Casino {
 	 * 
 	 * numero entre 1 y 20 para adivinar 5 intentos
 	 */
-
+	private static final int APUESTACOSTEFIJO = 150;
 	private int numeroSecreto;
 	/**
 	 * numero introducido por el usuario
@@ -23,7 +23,10 @@ public class AdivinarNumero extends Casino {
 	/**
 	 * numeor de intentos maximo 5
 	 */
-	private int intentosRestantes = 5;
+	private int intentosRestantes;
+
+	// si acierta pasa a verdadero
+	private boolean acertado;
 
 	//
 	// Constructors
@@ -115,52 +118,75 @@ public class AdivinarNumero extends Casino {
 	 * @param numeroUsuario
 	 * @param numeroSecreto
 	 */
-	public void adivinarNumero(int numeroIntroducido, int numeroSecreto, int intentosRestantes, int apuesta) {
+	public void adivinarNumero() {
 		Scanner sc = new Scanner(System.in);
-		apuesta = 100;
+
+		Entrenador entrenador = getEntrenador();
+		// comprobamos que haya entrenador
+		if (entrenador == null) {
+			System.out.println("No se encuentra al entrenador.");
+			return;
+		}
+
+		// comprobamos si tiene Pokedóllares el entrenador
+		if (!puedeApostar(APUESTACOSTEFIJO)) {
+			System.out.println("Pokedóllares insuficientes.");
+			return;
+		}
+
+		// cobramos la apuesta
+		costeApuesta(APUESTACOSTEFIJO);
+
+		// ramdom de 1 a 20 ( ambos incluidos)
+		this.numeroSecreto = (int) (Math.random() * 20) + 1;
+		// establecemos en 5 los intentos restantes
+		this.intentosRestantes = 5;
+		// y el boolean acertado en false
+		this.acertado = false;
+
 		// empezamos el bucle
 		do {
-			Entrenador entrenador = getEntrenador();
-			// comprobamos que haya entrenador
-			if (entrenador == null) {
-				System.out.println("No se encuentra al entrenador.");
-				break;
-			}
+			System.out.println("Intentos restantes: " + intentosRestantes);
+			System.out.println("Por favor introduzca un numero (1-20):");
 
-			// comprobamos si tiene Pokedóllares el entrenador
-			if (apuesta > entrenador.getPokedollares()) {
-				System.out.println("Pokedóllares insuficientes.");
-				break;
-			}
-			// comprobamos si hay intentos
-			if (intentosRestantes > 0) {
-				// try catch por si no se introduce un numero
-				try {
-					// ramdom de 1 a 20 ( ambos incluidos)
-					numeroSecreto = (int) (Math.random() * 20) + 1;
-					System.out.println("Por favor introduzca un numero (1-20):");
-					numeroIntroducido = Math.abs(sc.nextInt());
-				} catch (InputMismatchException a) {
-					System.out.println(a);
-					System.out.println("Error, no es un número");
+			// try catch por si no se introduce un numero
+			try {
+				this.numeroIntroducido = Math.abs(sc.nextInt());
+				// comrpobamos sia certo
+				if (numeroIntroducido == numeroSecreto) {
+					acertado = true;
+				} else {
+					intentosRestantes--;
+					if (intentosRestantes > 0) {
+						System.out.println("¡Número incorrecto! Sigue probando suerte");
+					}
 				}
-
+			} catch (InputMismatchException a) {
+				System.out.println("Error, no es un número válido.");
+				sc.next();
 			}
-			intentosRestantes--;
-			System.out.println("Número incorrecto, introduzca otro número:");
 			// realizamos bucle mientras el numero secreto no coincida con el introducido y
 			// si quedan intentos restantes
-		} while ((numeroSecreto != numeroIntroducido) & (intentosRestantes > 0));
+		} while (!acertado && (intentosRestantes > 0));
+
+		// cuando gane o agote intentos mostramos uno u otor mensaje y ñe pàgamos si
+		// gana
+		if (acertado) {
+			System.out.println("¡Enhorabuena! has acertado el número: " + numeroSecreto);
+			cantidadPremio(APUESTACOSTEFIJO);
+		} else {
+			System.out.println("Has perdido, el número era: " + numeroSecreto);
+		}
 
 	}
-	
+
 	@Override
 	// el costeApuesta descuenta los Pokedóllares al entrenador
 	public void costeApuesta(int apuesta) {
 		Entrenador entrenador = getEntrenador();
 		// si el entrenador existe le descontamos la cantidad de la apuesta
 		if (entrenador != null) {
-			entrenador.setPokedollares(entrenador.getPokedollares() - 100);
+			entrenador.setPokedollares(entrenador.getPokedollares() - apuesta);
 		}
 	}
 
@@ -173,20 +199,17 @@ public class AdivinarNumero extends Casino {
 			// y dependiendo del numero de intentos damos una u otra recompensa
 			if (intentosRestantes == 5) {
 				entrenador.setPokedollares(entrenador.getPokedollares() + 1000);
-			}
-			else if(intentosRestantes == 4){
+				System.out.println("Enhorabuena has ganado 1000 Pokedóllares");
+			} else if (intentosRestantes == 4) {
 				entrenador.setPokedollares(entrenador.getPokedollares() + 750);
 				System.out.println("Enhorabuena has ganado 750 Pokedóllares");
-			}
-			else if(intentosRestantes == 3){
+			} else if (intentosRestantes == 3) {
 				entrenador.setPokedollares(entrenador.getPokedollares() + 500);
 				System.out.println("Enhorabuena has ganado 500 Pokedóllares");
-			}
-			else if(intentosRestantes == 2){
+			} else if (intentosRestantes == 2) {
 				entrenador.setPokedollares(entrenador.getPokedollares() + 250);
 				System.out.println("Enhorabuena has ganado 250 Pokedóllares");
-			}
-			else{
+			} else {
 				entrenador.setPokedollares(entrenador.getPokedollares() + 0);
 			}
 		}
