744761a710c41c8592eaf8c6a3c22aba4816cf4b
Isaiasvm <isaiasvm1993@gmail.com>
2026-03-06 12:03:26 +0100

    conexion BBDD e IDs login.fml

--- src/bd/ConexionBBDD.java
+++ src/bd/ConexionBBDD.java
@@ -4,31 +4,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class ConexionBBDD {
+	
+	public Connection dataBaseLink;
+	
 
-	private static String url = "jdbc:mysql://localhost:3306/proyecto_pokemon";
-	private static String login = "root";
-	private static String password = "";
-	 
-
-	@SuppressWarnings("finally")
-	public static Connection getConexion() {
-		Connection connection = null;
+	public Connection getConexion() {
+		String dataBaseName = "proyecto_pokemoM";
+		String dataBaseUser = "root";
+		String dataBasePassword = "";
+		String url = "jdbc:mysql://localhost:3306/" + dataBaseName;
+		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection(url, login, password);
+			dataBaseLink = DriverManager.getConnection(url, dataBaseUser, dataBasePassword);
 			System.out.println("Conexión establecida");
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				connection.close();
-				System.out.println("Conexión cerrada");
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			return connection;
-		}
+		} 
+			return dataBaseLink;
 	}
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			// Cargar el login
-			Parent root = FXMLLoader.load(getClass().getResource("/view/.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
 
 			Scene scene = new Scene(root);
 
