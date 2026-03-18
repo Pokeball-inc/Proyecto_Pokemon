9882b1bd4f728bdff469b669b762c93d5fea15c0
Isaiasvm <isaiasvm1993@gmail.com>
2026-03-18 13:06:09 +0100

    ajustes conexionbbdd y metodos entrenadordao
    metodo agregado para el login y refrescar dinero

--- src/dao/EntrenadorDAO.java
+++ src/dao/EntrenadorDAO.java
@@ -12,26 +12,24 @@ import model.Entrenador;
 
 public class EntrenadorDAO {
 
-	// metodo para crear entrenador
-	public static void crearEntrenador(Connection con, Entrenador e)throws SQLException {
-		e.setIdEntrenador(obtenerIdEntrenador(con));
-		e.setPokedollares(generarPokedollares());
-		String sql = "INSERT INTO ENTRENADOR VALUES(?, ?, ?, ?, ?, ?, ?)";
-		
-		// introducir los values 
-		PreparedStatement ps = con.prepareCall(sql);
-		ps.setString(1, e.getNombreEntrenador());
-		//ps.setString(2, e.getGenero());
-		ps.setInt(3, e.getPokedollares());
-		//ps.setString(4, e.getTipoEntrenador());
-		ps.setString(5, e.getContrasena());
-		ps.setBoolean(6, e.getEsNPC());
-		ps.setString(7, e.getImgPosteriorEntrenador());
-		
-		
-		// para ejecutar y crear el entrenador
-		ps.executeUpdate();
-	}	
+	// metodo para crear entrenador ( id de entrenador autoincremente en bbdd)
+	public static void crearEntrenador(Connection con, Entrenador e) throws SQLException {
+        e.setPokedollares(generarPokedollares());
+        
+        // al ser AUTO_INCREMENT, no enviamos el ID
+        // especificamos las columnas para no tener errores de conteo
+        String sql = "INSERT INTO ENTRENADOR (NOM_ENTRENADOR, POKEDOLLARES, CONTRASENA) VALUES (?, ?, ?)";
+        
+        // establecemos en un try los datos del entrenador
+        try (PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, e.getNombreEntrenador());
+            ps.setInt(2, e.getPokedollares());
+            ps.setString(3, e.getContrasena()); // Password en texto plano
+
+            
+            ps.executeUpdate();
+        }
+    }	
 	
 	
 	// metodo para obtener id del entrenador, en nuestro caso tenemos el autoincrement, peor lo dejo como ejemplo
@@ -49,13 +47,14 @@ public class EntrenadorDAO {
 	}
 	
 	
+	// para generar la cantidad inicial de pokedollares entre 800 y 1000
 	private static int generarPokedollares() {
 		Random rd = new Random();
-		return rd.nextInt(800)+1000;
+		return rd.nextInt(201)+800;
 	}
 	
 	
-	
+	/*
 	// Obtener Pokedollares por id
 	private static void obtenerIDPokedollares(Connection con, Entrenador e) throws SQLException{
 		String sql = "SELECT ID_ENTRENADOR, POKEDOLLARES\r\n"
@@ -70,15 +69,50 @@ public class EntrenadorDAO {
 		
 		ResultSet rs = ps.executeQuery();
 		
-		while(rs.next())
+		while(rs.next()) {
+			e.set
+		}
 	}
+	*/
+	
+	// metodo de login
+	public static Entrenador login(Connection con, String nombre, String password) throws SQLException {
+        Entrenador entrenadorLogueado = null;
+        
+        String sql = "SELECT * FROM ENTRENADOR WHERE NOM_ENTRENADOR = ? AND CONTRASENA = ?";
+        
+        try (PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, nombre);
+            ps.setString(2, password); 
+            
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                	// creamos el entrenador y añadimos los datos de la base de datos
+                    entrenadorLogueado = new Entrenador();
+                    entrenadorLogueado.setIdEntrenador(rs.getInt("ID_ENTRENADOR"));
+                    entrenadorLogueado.setNombreEntrenador(rs.getString("NOM_ENTRENADOR"));
+                    entrenadorLogueado.setPokedollares(rs.getInt("POKEDOLLARES"));
+                    entrenadorLogueado.setContrasena(rs.getString("CONTRASENA"));
+                }
+            }
+        }
+        return entrenadorLogueado;
+    }
 	
 	
 	
-	
-	
-	
-	
+	// metodo para obtener dinero (por si se necesita refrescar solo eso)
+    public static void cargarPokedollares(Connection con, Entrenador e) throws SQLException {
+        String sql = "SELECT POKEDOLLARES FROM ENTRENADOR WHERE ID_ENTRENADOR = ?";
+        
+        try (PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, e.getIdEntrenador());
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                e.setPokedollares(rs.getInt("POKEDOLLARES"));
+            }
+        }
+    }
 	
 	
 	
