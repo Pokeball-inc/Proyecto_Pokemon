60db4a8549eb56a8ff0f941edbcd740cfc940915
Isaiasvm <isaiasvm1993@gmail.com>
2026-04-21 20:25:31 +0200

    combate
    cogi un mon ton nde cosas de mi proyecto de csharp osea ideas de loq ue necesitabamos, faltan mas seguro pero termino por hoy :D

--- src/model/Combate.java
+++ src/model/Combate.java
@@ -2,6 +2,7 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Class Combate
@@ -143,55 +144,315 @@ public class Combate {
 
   /**
    * metodo para iniciar el combate
-   * @param        entrenador
-   * @param        entrenadorRival
+   * @param entrenador, el entrenador jugador, nosotros
+   * @param entrenadorRival, el bot
    */
   public void empezarCombate(Entrenador jugador, Entrenador rival){
 	  this.setEntrenador(jugador);
 	    this.setEntrenadorRival(rival);
 	    
-	    // Buscamos el primer pokemon vivo de cada uno 
-	    this.setPokemonActualJugador(buscarPrimerPokemonVivo(jugador));
-	    this.setPokemonActualRival(buscarPrimerPokemonVivo(rival));
-	    
-	    // verificamos que ambos tengan alguien para pelear
-	    if (this.getPokemonActualJugador() != null && this.getPokemonActualRival() != null) {
-	        System.out.println("¡Combate listo! Sale " + this.getPokemonActualJugador().getNombrePokemon());
-	    } else {
-	        System.out.println("Error: Uno de los entrenadores no tiene Pokemon listos.");
+	    Pokemon pJugador = buscarPrimerPokemonVivo(jugador);
+	    Pokemon pRival = buscarPrimerPokemonVivo(rival);
+
+	    if (pJugador != null && pRival != null) {
+	        // reseteamos turnos estado
+	        pJugador.setTurnosEstadoRestantes(0);
+	        pRival.setTurnosEstadoRestantes(0);
+	        
+	        this.setPokemonActualJugador(pJugador);
+	        this.setPokemonActualRival(pRival);
+	        System.out.println("¡Combate listo!");
 	    }
   }
   
   /**
-   * Metodo para encontrar al primer pokemon vivo
-   * @param e, El entrenador,jugador o rival
-   * @return El primer Pokemon con vitalidad > 0
+   * metodo para encontrar al primer pokemon vivo
+   * @param e, el entrenador,jugador o rival
+   * @return el primer Pokemon con vitalidad > 0
    */
   private Pokemon buscarPrimerPokemonVivo(Entrenador e) {
-      // Recorremos el array de 6 pokemon del equipo
+      // recorremos el array de 6 pokemon del equipo
       for (Pokemon p : e.getEquipoPokemon()) {
-          // Si el hueco no está vacío y el pokemon tiene más de 0 de vida
+          // si el hueco no está vacio y el pokemon tiene más de 0 de vida
           if (p != null && p.getVitalidad() > 0) {
-              return p; // Devolvemos este pokemon para que sea el pokemonActual
+              return p; // devolvemos este pokemon para que sea el pokemonActual
           }
       }
-      return null; // Si todos están debilitados, devuelve null
+      return null; // si todos están debilitados, devuelve null
+  }
+  
+  
+  
+  /**
+   * cambia el pokemon actual del jugador por otro del equipo.
+   * @param indice El número del hueco del equipo, 0 a 5 por el array de 6 del equipo
+   * @return true si el cambio fue posible, false si el pokemon elegido no tiene vida
+   */
+  public boolean cambiarPokemonJugador(int indice) {
+      Pokemon elegido = this.getEntrenador().getEquipoPokemon()[indice];
+      
+      // verificamos que el hueco no sea nulo y que tenga vida
+      if (elegido != null && elegido.getVitalidad() > 0) {
+    	  // reseteamos los turnos de estado
+    	  elegido.setTurnosEstadoRestantes(0);
+          this.setPokemonActualJugador(elegido);
+          System.out.println("¡Has enviado a " + elegido.getNombrePokemon() + " a la batalla!");
+          return true;
+      } else {
+          System.out.println("No puedes elegir a ese Pokémon.");
+          return false;
+      }
   }
 
+  /**
+   * metodo para aplicar los efectos que se aplican a final de turno
+   * @param recibe el pokemon nuestro y el rival por parametro
+   * */
+  public void aplicarEfectosFinalDeTurno(Pokemon p, Pokemon rival) {
+     if (p == null || p.getVitalidad() <= 0) return;
+     // obtenemos la vida maxima para usarla 
+     int psTotales = p.getVitalidadMaxima();
+
+     switch (p.getEstadoActual()) {
+         case QUEMADO:
+             // pierde 1/16 de sus PS totales
+             p.setVitalidad(p.getVitalidad() - (psTotales / 16));
+             break;
+
+         case ENVENENADO:
+             // pierde 1/8 de sus PS totales
+             p.setVitalidad(p.getVitalidad() - (psTotales / 8));
+             break;
+
+         case GRAVEMENTEENMVENEDADO:
+             // daño aumenta en 1/16 cada turno (TODO usar turnosEstadoRestantes para el contador)
+             int turnos = p.getTurnosEstadoRestantes() + 1;
+             p.setTurnosEstadoRestantes(turnos);
+             p.setVitalidad(p.getVitalidad() - (psTotales * turnos / 16));
+             break;
+
+         case HELADO:
+             // pierde 1/16 de sus PS totales
+             p.setVitalidad(p.getVitalidad() - (psTotales / 16));
+             break;
+
+         case MALDITO:
+             // pPierde 1/4 de sus PS totales
+             p.setVitalidad(p.getVitalidad() - (psTotales / 4));
+             break;
+
+         case DRENADORAS:
+             // pierde 1/8 y los recupera el rival
+             int robo = psTotales / 8;
+             p.setVitalidad(p.getVitalidad() - robo);
+             // Si el rival existe, recupera la vida robada
+             if (rival != null) {
+                 rival.setVitalidad(rival.getVitalidad() + robo);
+             }
+             break;
+
+         case CANTOMORTAL:
+             // se debilita a los 3 turnos
+             p.setTurnosEstadoRestantes(p.getTurnosEstadoRestantes() + 1);
+             if (p.getTurnosEstadoRestantes() >= 3) {
+                 p.setVitalidad(0);
+                 p.setEstadoActual(Estados.DEBILITADO);
+             }
+             break;
+             
+         case DORMIDO:
+             // Se despertara despues de 1-3 turnos
+             p.setTurnosEstadoRestantes(p.getTurnosEstadoRestantes() - 1);
+             if (p.getTurnosEstadoRestantes() <= 0) {
+                 p.setEstadoActual(Estados.SANO);
+                 System.out.println(p.getNombrePokemon() + " se ha despertado.");
+             }
+             break;
+
+         default:
+             break;
+     }
+  }
+
+/**
+ * Comprueba si el estado actual del Pokémon le permite realizar un movimiento.
+ * @param recibe el pokemon afectado cvomo parametro
+ * @return true si puede atacar, false si el estado lo impide
+ * */
+public boolean puedeAtacarEsteTurno(Pokemon p) {
+	Random r = new Random();
+    Estados estado = p.getEstadoActual();
+
+    switch (estado) {
+        case PARALIZADO:
+            // 12,5% de probabilidad de no atacar
+            if (r.nextDouble() < 0.125) {
+                System.out.println(p.getNombrePokemon() + " está paralizado y no puede moverse.");
+                return false;
+            }
+            break;
+
+        case DORMIDO:
+            // no puede atacar mientras duerme
+            System.out.println(p.getNombrePokemon() + " está profundamente dormido...");
+            return false;
+
+        case CONGELADO:
+            // 20% de probabilidad de descongelarse
+            if (r.nextDouble() < 0.20) {
+                p.setEstadoActual(Estados.SANO);
+                System.out.println("¡" + p.getNombrePokemon() + " se ha descongelado!");
+                return true;
+            }
+            System.out.println(p.getNombrePokemon() + " está congelado y no puede atacar.");
+            return false;
+
+        case AMEDENTRADO:
+            // no puede atacar este turno, luego se cura solo
+            System.out.println(p.getNombrePokemon() + " ha retrocedido y no puede atacar.");
+            p.setEstadoActual(Estados.SANO); 
+            return false;
+
+        case SOMNOLIENTO:
+            // 50% de probabilidad de no atacar
+            if (r.nextDouble() < 0.50) {
+                System.out.println(p.getNombrePokemon() + " tiene mucho sueño y no puede atacar.");
+                return false;
+            }
+            break;
+
+        case CONFUSO:
+            // 1/3 de probabilidad de herirse a sí mismo
+            if (r.nextInt(3) == 0) {
+                int danio = p.getVitalidadMaxima() / 10; // Daño por confusión (10% vida max)
+                p.setVitalidad(p.getVitalidad() - danio);
+                System.out.println("¡" + p.getNombrePokemon() + " está tan confuso que se ha herido a sí mismo!");
+                return false;
+            }
+            break;
+
+        case ENAMORADO:
+            // 1/4 de probabilidad de no atacar
+            if (r.nextInt(4) == 0) {
+                System.out.println(p.getNombrePokemon() + " está enamorado y no puede atacar.");
+                return false;
+            }
+            break;
+
+        case DEBILITADO:
+            return false;
+            
+        default:
+            return true;
+    }
+    return true;
+}
+  
+  /**
+   * metodo para tras la muerte de un pokemon  preparar el cambio.
+   * @param esJugador, true si el pokemon muerto es el nuestro
+   * @return true, si al entrenador le quedan mas pokemon para seguir peleando
+   */
+  public boolean puedeContinuar(boolean esJugador) {
+      if (esJugador) {
+          // aumentamos el contador de KOs del jugador
+          this.setPokemonKOEntrenador(this.getPokemonKOEntrenador() + 1);
+          
+          // comprobamos si queda alguien vivo en el equipo
+          return buscarPrimerPokemonVivo(this.getEntrenador()) != null;
+      } else {
+          // logica para el rival, que sacara el primero vivo
+          this.setPokemonKORival(this.getPokemonKORival() + 1);
+          Pokemon siguienteRival = buscarPrimerPokemonVivo(this.getEntrenadorRival());
+          
+          if (siguienteRival != null) {
+              this.setPokemonActualRival(siguienteRival);
+              return true;
+          }
+          return false;
+      }
+  }
+
+  
+  /**
+   * metodo para calcular y transferir los Pokedollares del perdedor al ganador
+   * @param ganador ,el entrenador que ha gando
+   * @param perdedor, el entrenador que ha eprdido
+   */
+  public void transferirPokedollares(Entrenador ganador, Entrenador perdedor) {
+      // calculamos el tercio del dinero del perdedor
+      int botin = perdedor.getPokedollares() / 3;
+      
+      // actualizamos los  Pokedollares de ambos
+      perdedor.setPokedollares(perdedor.getPokedollares() - botin);
+      ganador.setPokedollares(ganador.getPokedollares() + botin);
+      
+      System.out.println("¡Botín de combate! " + ganador.getNombreEntrenador() + 
+                         " ha ganado " + botin + " Pokedollares.");
+  }
+  
+  /**
+   * metodo que reparte experiencia a todo el equipo respecto al nivel del rival derrotado
+   */
+  public void repartirExperienciaEquipo(Pokemon rivalDerrotado) {
+      Pokemon[] equipo = this.getEntrenador().getEquipoPokemon();
+      // recorremos el equipo y por cada pokemon
+      for (Pokemon p : equipo) {
+          if (p != null) {
+              // formula del enunciado :D
+              int expGanada = (p.getNivel() + (rivalDerrotado.getNivel() * 10)) / 4;
+
+              // añadimos la experiencia ganada generada por la formula
+              p.ganarExperiencia(expGanada);
+          }
+      }
+  }
 
   /**
    * metodo para retirarse del combate
    */
-  public void retirarse()
-  {
+  public void retirarse(){System.out.println("--- EL JUGADOR SE HA RETIRADO ---");
+  
+  // sumamos la derrota al marcador
+  this.getEntrenador().sumarDerrota();
+  
+  // transferimos los Pokedollares al rival
+  transferirPokedollares(this.getEntrenadorRival(), this.getEntrenador());
+  
+  System.out.println("Has abandonado el combate. Has perdido " + 
+                     (this.getEntrenador().getNombreEntrenador()) + " Pokedollares.");
   }
 
 
   /**
-   * metodo para finalizar combate
+   * metodo para finalizar combate y establecer ganador
    */
-  public void finalizarCombate()
-  {
+  public void finalizarCombate(){
+	  System.out.println("EL COMBATE HA FINALIZADO");
+      
+      // si ganamos nosotros
+      if (this.getPokemonKORival() >= 6 || buscarPrimerPokemonVivo(this.getEntrenadorRival()) == null) {
+            
+          System.out.println("¡ENHORABUENA! Has ganado el combate.");
+          this.getEntrenador().sumarVictoria();
+            
+          //  hhacemos el reparto de Pokedollares
+          transferirPokedollares(this.getEntrenador(), this.getEntrenadorRival());
+            
+          // añadimos la xp a todo el equipo en base al nivel del ultimo pokemon del rival
+          if (this.getPokemonActualRival() != null) {
+              repartirExperienciaEquipo(this.getPokemonActualRival());
+          }
+
+      } 
+      // si gana el rival
+      else {
+          System.out.println("¡DERROTA! Te has quedado sin Pokémon.");
+          this.getEntrenador().sumarDerrota();
+            
+          // añadimos el dienro al rival
+          transferirPokedollares(this.getEntrenadorRival(), this.getEntrenador());
+      }
   }
   
   /**
