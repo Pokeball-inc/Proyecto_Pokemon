11f6c05f1fceb56bbad4dea184ce26d19caaf234
Elyass Douma <elyassdouma14@hotmail.com>
2026-04-22 05:55:04 +0200

    pokedex a inventario

--- src/controller/InventarioController.java
+++ src/controller/InventarioController.java
@@ -5,7 +5,7 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class PokedexController implements Initializable {
+public class InventarioController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
     }
