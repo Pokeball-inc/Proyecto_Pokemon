c753b68dc3ebd8598215845fce942425336a4021
Elyass Douma <elyassdouma14@hotmail.com>
2026-04-24 13:54:45 +0200

    Función de comprar en el inventario ya implementada + aviso en caso de fallo

--- src/controller/InventarioController.java
+++ src/controller/InventarioController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import bd.ConexionBBDD;
+import dao.EntrenadorDAO;
+import dao.InventarioDAO;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -9,12 +11,18 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import model.*;
@@ -22,11 +30,9 @@ import model.*;
 import java.io.File;
 import java.net.URL;
 import java.sql.Connection;
-import java.util.Arrays;
 import java.util.ResourceBundle;
 
-import static dao.InventarioDAO.cargarInventario;
-import static dao.InventarioDAO.cargarObjetosTotales;
+import static dao.InventarioDAO.*;
 
 public class InventarioController implements Initializable {
 
@@ -49,6 +55,12 @@ public class InventarioController implements Initializable {
     private Text txtPrecioObjeto;
     @FXML
     private Text txtDescObjeto;
+    @FXML
+    private Pane usarObjeto;
+    @FXML
+    private Pane comprarObjeto;
+    @FXML
+    private Text textSaldo;
 
     private Connection con;
 
@@ -58,7 +70,7 @@ public class InventarioController implements Initializable {
         // Cargar objetos al entrar
         cargarObjetos();
 
-        cargarObjetosEntrenador();
+        cargarInfoObjeto(model.ObjetosTotales.todosLosObjetos.get(0));
     }
 
     /**
@@ -74,6 +86,8 @@ public class InventarioController implements Initializable {
             // Limpiar la caja por si tenia algo antes
 
             cajaObjetos.getChildren().clear();
+
+            textSaldo.setText(String.valueOf("Disponibles: " + entrenadorActual.getPokedollares()) + " Pd");
 
 
             // Instanciamos la conexion con la BBDD
@@ -141,19 +155,7 @@ public class InventarioController implements Initializable {
                 }
             }
 
-            cargarInfoObjeto(ObjetosTotales.todosLosObjetos.get(0));
 
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    // Method para cargar la lista de objetos del entrenador
-
-    public void cargarObjetosEntrenador() {
-        try {
-            System.out.println(entrenadorActual.getInventario().toString());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -178,7 +180,7 @@ public class InventarioController implements Initializable {
 
         // Ahora el precio del objeto
 
-        txtPrecioObjeto.setText(String.valueOf(objeto.getPrecio()));
+        txtPrecioObjeto.setText(String.valueOf(objeto.getPrecio()) + " Pd");
 
         // Ahora la descripción
 
@@ -191,7 +193,85 @@ public class InventarioController implements Initializable {
 
         // El sum devuelve 0 si no encuentra nada en el stream, mejor que un if xd
 
-        txtcantidadObjeto.setText(String.valueOf(cantidad));
+        txtcantidadObjeto.setText(String.valueOf("x" + cantidad));
+
+
+        // Modificar botones de Comprar y Usar en función de la cantidad que poseas
+
+        if (cantidad > 0) {
+
+
+            usarObjeto.setVisible(true);
+
+            /// El botón de comprar centrado
+
+            Rectangle rectComprar = (Rectangle) comprarObjeto.getChildren().get(0);
+            comprarObjeto.setLayoutX(33);
+            comprarObjeto.setLayoutY(404);
+            comprarObjeto.setPrefHeight(Region.USE_COMPUTED_SIZE);
+            comprarObjeto.setPrefWidth(Region.USE_COMPUTED_SIZE);
+            rectComprar.setWidth(102);
+            rectComprar.setHeight(37);
+            rectComprar.setLayoutX(0);
+            rectComprar.setLayoutY(0);
+
+
+
+
+        } else {
+
+            usarObjeto.setVisible(false);
+
+            /// El botón de comprar centrado
+
+            Rectangle rectComprar = (Rectangle) comprarObjeto.getChildren().get(0);
+            comprarObjeto.setLayoutX(111);
+            comprarObjeto.setLayoutY(405);
+            comprarObjeto.setPrefHeight(37);
+            comprarObjeto.setPrefWidth(102);
+            rectComprar.setWidth(151);
+            rectComprar.setHeight(37);
+            rectComprar.setLayoutX(-26);
+            rectComprar.setLayoutY(0);
+
+        }
+
+        ///  Evento de mouse para comprar
+
+        comprarObjeto.setOnMouseClicked(event -> {
+            if (entrenadorActual.getPokedollares() >= objeto.getPrecio()) {
+
+                int nuevoSaldo = entrenadorActual.getPokedollares() - objeto.getPrecio();
+
+                entrenadorActual.setPokedollares(nuevoSaldo);
+
+                entrenadorActual.getInventario().añadirObjeto(objeto, 1);
+
+                new EntrenadorDAO().actualizarPokedollares(entrenadorActual.getIdEntrenador(), nuevoSaldo);
+                InventarioDAO.actualizarInventario(con);
+
+                System.out.println("Has comprado "+ objeto.getNombreObjeto() +" por "+ String.valueOf(objeto.getPrecio()) + "!");
+
+                /// Recargar
+
+                cargarObjetos();
+
+                ///  Mostrar último objeto comprado
+                cargarInfoObjeto(objeto);
+            } else {
+
+                /// Aviso de saldo insuficiente
+
+                mostrarAlerta("¡Error al comprar objeto!", "No tienes suficiente saldo para realizar esta compra",
+                        "Saldo necesario: "+ String.valueOf(objeto.getPrecio()) + "\nTu saldo: " + String.valueOf(entrenadorActual.getPokedollares()), Alert.AlertType.WARNING );
+
+                /// Y mostrarlo
+
+
+
+
+            }
+        });
 
     }
 
@@ -216,6 +296,50 @@ public class InventarioController implements Initializable {
         return vistaImagen;
     }
 
+
+    //Metodo para mostrar pop-up con mensajes
+
+    private void mostrarAlerta(String titulo, String cabecera, String contenido, Alert.AlertType tipo) {
+        Alert alerta = new Alert(tipo); //ventana del tipo que pasemos informacion, advertencia... lo que queramos
+        //Textos de la ventana
+        alerta.setTitle(titulo); //Texto barra superior
+        alerta.setHeaderText(cabecera); //Texto grande en negrita
+        alerta.setContentText(contenido); //Texto en pequeño
+
+        //Parte visual de la alerta
+        //Icono central
+        try {
+            String rutaIcono = new File("imgs/Login/Login-icon.png").toURI().toString();
+            ImageView icono = new ImageView(new Image(rutaIcono));
+            icono.setFitHeight(50);
+            icono.setFitWidth(50);
+            alerta.setGraphic(icono); //sustituye el que hay por defecto
+        } catch (Exception e) {
+            System.out.println("No se puede cargar el icono personalizado");
+        }
+
+        DialogPane dialogPane = alerta.getDialogPane(); //panel principal de alerta
+
+        //Icono esquina superior izquierda
+        try {
+            Stage stage = (Stage) dialogPane.getScene().getWindow();
+            stage.getIcons().add(new Image(new File("imgs/Login/Login-icon.png").toURI().toString()));
+        } catch (Exception e) {
+            System.out.println("No se puede cargar el icono personalizado");
+        }
+
+        //Para diferenciar entre exito y fracaso
+        if(tipo == Alert.AlertType.WARNING) {
+            dialogPane.getStylesheets().add(getClass().getResource("/view/captura/alertas2.css").toExternalForm());
+        } else {
+            dialogPane.getStylesheets().add(getClass().getResource("/view/captura/alertas.css").toExternalForm()); //Enlace con archivo alertas css personalizado
+        }
+
+
+
+        alerta.showAndWait(); //muestra la ventana en la pantalla y para el codigo hasta que se acepta o se cierra la ventana
+
+    }
 
     //Boton de salir
     @FXML
